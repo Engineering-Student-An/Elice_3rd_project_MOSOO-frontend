9521d6cc113d56fc683f1763562fb75416e081c0
[CLOUD4] 안창민 <chm20060@gmail.com>
2024-11-27 22:48:13 +0900

    feat: #3 채팅 관련 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import { Header, Footer } from "./fragments";
+import './assets/css/LineIcons.2.0.css';
+import './assets/css/animate.css';
+import './assets/css/tiny-slider.css';
+import './assets/css/glightbox.min.css';
+import './assets/css/main.css';
+
+import { Header, Footer } from "./components";
+import { ChatRoomList, ChatRoom } from "./pages/chatting";
 
 const App = () => {
   return (
@@ -12,6 +19,9 @@ const App = () => {
             <Header />
             <main className="flex-grow-1">
                 <Routes>
+
+                    <Route path="/chatrooms" element={<ChatRoomList />} /> {/* 채팅방 목록 화면 */}
+                    <Route path="/chatroom/:chatRoomId" element={<ChatRoom />} /> {/* 채팅방 화면 */}
                 </Routes>
             </main>
             <Footer />
