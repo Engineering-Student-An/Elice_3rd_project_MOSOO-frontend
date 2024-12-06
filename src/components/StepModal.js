5fdeec05563f8e5ab7a6e02c95b7824c12c08017
[CLOUD4] 홍민영 <47109@naver.com>
2024-12-06 12:48:55 +0900

    feat: #22 offerPosts navigate 및 데이터 전달

--- src/components/StepModal.js
+++ src/components/StepModal.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Step1 from "./Address";
 import Step2 from "../pages/category/components/SecondCategory";
 import Step3 from "../pages/category/components/ThirdCategory";
 import "./StepModal.css";
+
 
 const StepModal = ({ category_id, onClose }) => {
   const [step, setStep] = useState(1); // 단계 상태
   const [selectedSecondcategory, setSelectedSecondcategory] = useState(null); // 선택된 중분류 상태
   const [address, setAddress] = useState(null);
   const [thirdCategory, setThirdCategory] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
     console.log("step 상태가 변경됨:", step);
@@ -58,6 +61,14 @@ const StepModal = ({ category_id, onClose }) => {
     console.log("검색 버튼 클릭됨");
     console.log("주소 정보:", address || "주소가 설정되지 않았습니다.");
     console.log("ThirdCategory ID:", thirdCategory?.category_id || "소분류가 설정되지 않았습니다.");
+
+    navigate("/offerPosts", {
+      state: {
+        address: address,
+        thirdCategory: thirdCategory?.category_id
+      },
+    });
+
     onClose();
   };
 
